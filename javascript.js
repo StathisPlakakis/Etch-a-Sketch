2e561a175f641bc94b96b2632d725df9340d8aa0
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-09 15:01:58 +0200

    Add the final grid
    In the container i put 16 grid divs(display:flex), in each div i put 16 divs

--- javascript.js
+++ javascript.js
@@ -1,11 +1,19 @@
 const container = document.querySelector(".container");
+
 for (let j = 0; j < 16; j++) {
     const div = document.createElement("div");
     div.classList.add("grid");
-    div.style.height = "15px";
-    div.style.width = "15px";
-    div.style.backgroundColor = "grey";
-    div.style.border = "1px solid black";
     container.appendChild(div);
-    console.log(j)
 }
+
+const grids = document.querySelectorAll(".grid");
+grids.forEach((grid) => {
+    for (let i = 0; i < 16; i++) {
+        const div = document.createElement("div");
+        div.style.height = "15px";
+        div.style.width = "15px";
+        div.style.backgroundColor = "white";
+        div.style.border = "1px solid black";
+        grid.appendChild(div);
+    }
+})

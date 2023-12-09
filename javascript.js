09ce9792c06349760b6ac5aff04b65f6698185b3
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-09 16:06:23 +0200

    Add event listener to every square div
    In mouseover event the specific square div is changing color

--- javascript.js
+++ javascript.js
@@ -10,10 +10,17 @@ const grids = document.querySelectorAll(".grid");
 grids.forEach((grid) => {
     for (let i = 0; i < 16; i++) {
         const div = document.createElement("div");
+        div.classList.add("square");
         div.style.height = "15px";
         div.style.width = "15px";
         div.style.backgroundColor = "white";
         div.style.border = "1px solid black";
         grid.appendChild(div);
     }
+})
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    })
 })

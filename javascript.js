683119146119dffb055d49700ffb1c40dbaf6adc
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-10 10:18:57 +0200

    Make the dimensions of grid dynamic
    The grid itself is stable(700px) while the dimensions of the squares dynamic

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
+let x = 700;
 const container = document.querySelector(".container");
 container.style.backgroundColor = 'red';
-container.style.height = '500px';
-container.style.width = '500px';
+container.style.height = `${x}px`;
+container.style.width = `${x}px`;
 
 for (let j = 0; j < 16; j++) {
     const div = document.createElement("div");
@@ -14,8 +15,8 @@ grids.forEach((grid) => {
     for (let i = 0; i < 16; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
-        div.style.height = "15px";
-        div.style.width = "15px";
+        div.style.height = `${x / 16}px`;
+        div.style.width = `${x / 16}px`;
         div.style.backgroundColor = "white";
         div.style.border = "1px solid black";
         grid.appendChild(div);

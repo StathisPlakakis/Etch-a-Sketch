80fbfe61300f3079909e1b70410073b1ca2039fe
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-10 11:54:59 +0200

    Add event listener to the button
    Clicking the button lets you choose dimensions of the new grid

--- javascript.js
+++ javascript.js
@@ -1,35 +1,56 @@
-let x = 700;
+let x = 960;
 const container = document.querySelector(".container");
-container.style.backgroundColor = 'red';
 container.style.height = `${x}px`;
 container.style.width = `${x}px`;
 
-for (let j = 0; j < 16; j++) {
-    const div = document.createElement("div");
-    div.classList.add("grid");
-    container.appendChild(div);
-}
-
-const grids = document.querySelectorAll(".grid");
-grids.forEach((grid) => {
-    for (let i = 0; i < 16; i++) {
-        const div = document.createElement("div");
-        div.classList.add("square");
-        div.style.height = `${x / 16}px`;
-        div.style.width = `${x / 16}px`;
-        div.style.backgroundColor = "white";
-        div.style.border = "1px solid black";
-        grid.appendChild(div);
-    }
-})
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    })
-})
+createGrid(16);
 
 const button = document.createElement('button');
 button.textContent = "New Grid";
 button.classList.add("btn");
 document.body.insertBefore(button, container);
+button.addEventListener('click', () => {
+    let newGrid = parseInt(prompt('Choose the number of squares per side for the new grid.'))
+
+    while ( newGrid <= 0 ||
+        newGrid > 100 ||
+        isNaN(newGrid) === true) {
+            newGrid = prompt('You should choose a NUMBER between 1 & 100');
+        }
+    
+    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    createGrid(newGrid);
+    
+});
+
+function createGrid(n) {
+    for (let j = 0; j < n; j++) {
+        const div = document.createElement("div");
+        div.classList.add("grid");
+        container.appendChild(div);
+    }
+
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+    for (let i = 0; i < n; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        div.style.height = `${x / n}px`;
+        div.style.width = `${x / n}px`;
+        div.style.backgroundColor = "white";
+        div.style.border = "1px solid black";
+        grid.appendChild(div);
+    }
+
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    })
+})
+})
+}
